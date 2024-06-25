6439b2819306f14cfceecda3981507c0955a5fee
Austin <lyvuaustin@gmail.com>
2024-06-24 19:27:12 -0500

    Added PUT endpoint to update name

--- src/student/queries.js
+++ src/student/queries.js
@@ -1,8 +1,9 @@
-const getStudents = "SELECT * FROM students";
+const getStudents = "SELECT * FROM students ORDER BY id";
 const getStudentById = "SELECT * FROM students WHERE id = $1";
 const checkEmailExists = "SELECT s FROM students s WHERE s.email = $1";
 const addStudent = "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
 const removeStudent = "DELETE FROM students WHERE id = $1";
+const updateStudent = "UPDATE students SET name = $1 WHERE id = $2";
 
 module.exports = {
     getStudents,
@@ -10,4 +11,5 @@ module.exports = {
     checkEmailExists,
     addStudent,
     removeStudent,
+    updateStudent,
 }
