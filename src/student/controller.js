6b6d86c5ad2061d211b37be8e3ae3582794bfb2e
Austin <lyvuaustin@gmail.com>
2024-06-27 22:41:20 -0500

    Added Coonstants and Modules

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,97 +1,53 @@
 const { response } = require('express');
 const pool = require('../../db');
 const queries = require('./queries');
-const c = require('./constants');
+const m = require('./modules')
 const { Validator } = require('jsonschema');
 
 // Create a Validator instance
 const validator = new Validator();
 
-const failResponse = {
-    message: "Failed to Grabbed Data"
-}
-
-// Format for expected request body
-const schema = {
-    type: 'object',
-    properties: {
-        name: { type: ['string', 'null'] },
-        email: { type: ['string', 'null'], format: 'email' },
-        age: { type: ['integer', 'null'], minimum: 0 },
-        dob: { type: ['string', 'null'], format: 'date' }
-    },
-    required: ['name', 'email', 'age', 'dob']
+// Retrieves full table of all students
+const getStudents = (req, res) => {
+    m.QueryAllStudents(res, queries.getStudents);
 };
 
-const getStudents = (req, res) => {
-
-    pool.query(queries.getStudents, (error, results) => {
-
-        if (error) throw error;
-
-        const response = {
-            message: c.SUCCESS_FILE_GRAB,
-            data: results.rows
-        }
-
-        return res.status(200).json(response);
-    });
-}
-
 const getStudentById = (req, res) => {
- 
+    
     const id = parseInt(req.params.id);
 
-    pool.query(queries.getStudentById, [id], (error, results) => {
+    QueryById(res, queries.getStudentById, id);
 
-        try {
-            const successResponse = {
-                message: "Successfully Grabbed Data",
-                data: results.rows
-            }
-
-            if (error) throw error;
-
-            if (results.rows.length == 0) {
-                return res.status(200).json( { message: "There are no students with that Id"});
-            }
-    
-            return res.status(200).json(successResponse);
-        }
-        catch (error) {
-            return res.status(500).json(failResponse);
-        }
-    });
 };
 
 const addStudent = (req, res) => {
     const { name, email, age, dob } = req.body;
 
-    const validation = validator.validate(req.body, schema);
+    const validation = validator.validate(req.body, c.SCHEMA);
 
     if (!validation.valid) {
-        return res.status(422).json({ message: "Incorrect Request Body" } );
+        return res.status(422).json({ message: c.INVALID_BODY_REQ } );
     }
 
     // Check if email exists
     pool.query(queries.checkEmailExists, [email], (error, results) => {
         if (error) {
-            return res.status(500).json( { message: "Error checking email." } );
+            return res.status(500).json( { message: c.EMAIL_ERROR } );
         }
 
         if (results.rows.length) {
-            return res.json({ message: "Email already exists." });
+            return res.json({ message: c.EMAIL_EXST });
         }
 
         // Add student to db
         pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
             if (error) {
-                return res.status(500).json( {message: "Error adding student." } )
+                return res.status(500).json( {message: c.ADD_ERROR } )
             };
 
             pool.query(queries.getStudentByName, [name], (error, getResult) => {
                 const response = {
-                    message: "Student Successfully Created!",
+                    message: c.SUCCESS_ADD,
                     data: getResult.rows
                 };
 
@@ -113,16 +69,16 @@ const removeStudent = (req, res) => {
 
             const noStudentFound = !results.rows.length;
             if (noStudentFound) {
-                return res.json( {message: "Student does not exist."} )
+                return res.json( {message: c.INVALID_STUDENT_ID} )
             };
 
             pool.query(queries.removeStudent, [id], (error, results) => {
                 if (error) throw error;
-                return res.status(200).json( { message: "Student Removed Successfully!" } );
+                return res.status(200).json( { message: c.SUCCESS_REMOVE } );
             })
         }
         catch {
-            return res.status(500).json( { message: "Id is too long!" } );
+            return res.status(500).json( { message: c.ID_ERROR } );
         }
     });
 };
@@ -140,17 +96,17 @@ const updateStudent = (req, res) => {
     const id = parseInt(req.params.id);
 
     // Validate the response body against the schema
-    const validation = validator.validate(req.body, schema);
+    const validation = validator.validate(req.body, c.SCHEMA);
 
     if (!validation.valid) {
-        return res.status(422).json({ message: "Incorrect Request Body" } );
+        return res.status(422).json({ message: c.INVALID_BODY_REQ } );
     }
 
     // Check if student is in db
     pool.query(queries.getStudentById, [id], (error, results) => {
         const noStudentFound = !results.rows.length;
         if (noStudentFound) {
-            return res.json( { message: "Student does not exist." } );
+            return res.json( { message: c.INVALID_STUDENT_ID } );
         }
 
         // Gets original data
@@ -171,10 +127,10 @@ const updateStudent = (req, res) => {
             pool.query(queries.updateStudent, [name, email, age, dob, id], (error, results) => {
                 if (error) {
                     console.error('Error executing SQL Query', error);
-                    return res.status(500).json({ message: "Internal Server Error"} );
+                    return res.status(500).json({ message: INT_SERVER_ERROR } );
                 };
 
-                return res.status(200).json({ message: "Student Updated Successfully!" } );
+                return res.status(200).json({ message: c.SUCCESS_UPDT } );
             });
         });
     });
